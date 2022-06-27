b682eb5b156f0c7f629d6445bed5c2d49d8d6670
Alassane NIANG <adniang75@gmail.com>
2022-06-27 19:20:45 +0200

    Implemented Observable Using just

---
+++ src/main/java/com/alassaneniang/rxjava/observables/ObservableUsingJust.java
@@ -0,0 +1,16 @@
+package com.alassaneniang.rxjava.observables;
+
+import io.reactivex.rxjava3.core.Observable;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j(topic = "ObservableUsingJust")
+public class ObservableUsingJust {
+
+    public static void main(String[] args) {
+
+        Observable.just("a", "b", "c", "d", "e", "f", "g", "h", "i", "j")
+                .subscribe(new DemoObserver<>());
+
+    }
+
+}
