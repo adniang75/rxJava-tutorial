f86533351d377692da7889d3bb003e36466fbbc3
Alassane NIANG <adniang75@gmail.com>
2022-06-27 20:10:05 +0200

    Implemented Observable Using defer

---
+++ src/main/java/com/alassaneniang/rxjava/observables/ObservableUsingDefer.java
@@ -0,0 +1,18 @@
+package com.alassaneniang.rxjava.observables;
+
+import com.alassaneniang.rxjava.utils.RxUtils;
+import io.reactivex.rxjava3.core.Observable;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j(topic = "ObservableUsingDefer")
+public class ObservableUsingDefer {
+
+    public static void main(String[] args) {
+        Observable<Integer> observableUsingDefer = Observable.defer(
+                () -> Observable.fromIterable(RxUtils.positiveNumbers(5))
+        );
+        observableUsingDefer.subscribe(new DemoObserver<>());
+        observableUsingDefer.subscribe(new DemoObserver<>());
+    }
+
+}
