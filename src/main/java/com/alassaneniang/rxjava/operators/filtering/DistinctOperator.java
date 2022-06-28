e517755d736ee2563f31e3c63481fdd83725bac0
Alassane NIANG <adniang75@gmail.com>
2022-06-28 10:00:16 +0200

    Operators - Implemented Filtering (Distinct)

---
+++ src/main/java/com/alassaneniang/rxjava/operators/filtering/DistinctOperator.java
@@ -0,0 +1,19 @@
+package com.alassaneniang.rxjava.operators.filtering;
+
+import com.alassaneniang.rxjava.observables.DemoObserver;
+import io.reactivex.rxjava3.core.Observable;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class DistinctOperator {
+
+    public static void main(String[] args) {
+
+        log.info("Distinct Operator");
+        Observable.just(1, 2, 3, 2, 4, 1, 2, 3, 4, 5)
+                .distinct()
+                .subscribe(new DemoObserver<>());
+
+    }
+
+}
